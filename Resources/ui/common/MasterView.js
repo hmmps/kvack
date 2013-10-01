19f80310b6522f42d85cca6a2d2ccf5d1281d9c5
Hampus Ahlm Brenander <hmmps@skenkonst.se>
2013-10-01 21:33:13 +0200

    Rename method loadEpisodesTable to addEpisodesToTable

--- Resources/ui/common/MasterView.js
+++ Resources/ui/common/MasterView.js
@@ -1,129 +1,131 @@
 //Master View Component Constructor
 function MasterView() {
-	//create object instance, parasitic subclass of Observable
-	var self = Ti.UI.createView({
-		backgroundColor:'white'
-	});
-	
-	// load Feed module
-	var Feed = require('Services/feed');
-	var episodesFeed = new Feed();
-	
-	// Load database module
-	var EpisodesDb = require('Services/db');
 
-        // Load episodeRow module
-        var EpisodeRow = require('ui/common/episodeRow');
+    //create object instance, parasitic subclass of Observable
+    var self = Ti.UI.createView({
+        backgroundColor:'white'
+    });
 
-        // Load module for episodeRow
-        var EpisodeRow = require('ui/common/episodeRow');
-	
-	// Create container for episodes
-	var episodesList = [];
-	
-	// Create tableView for episodes
-	var table = Ti.UI.createTableView({});
-	self.add(table);
-	
-	// Update Feeds
-	Ti.API.info('Fetching remote feed');
-	episodesFeed.fetchRemoteFeed();
-	
-	Ti.App.addEventListener('fetchRemoteFeedFinished', function(e){
-		
-		// Get the date of the newest stored episode
-		var episodes = new EpisodesDb();
-		var latestUpdate = episodes.getLatestUpdate();
-		
-		// get the new episodes
-		Ti.API.debug('getting the new episodes');
-		newEpisodes = episodesFeed.getNewEpisodes(latestUpdate);
-		
-		Ti.API.debug(newEpisodes.length + ' new episodes to save');
-		
-		// Save new episodes to database
-		for(var i = 0, j = newEpisodes.length; i < j; i++){
-			// newEpisodes is an array with DOMElements
-			var itemNode = newEpisodes[i];
-			
-			// Feed.getEpisodeDetails returns an objects
-			var episode = episodesFeed.getEpisodeDetails(itemNode);
-			
-			Ti.API.debug(episode.title);
-			
-			episodes.saveEpisode(episode);
-		}
-		
-		// Remember to close the DB
-		episodes.close();
-		
-		Ti.App.fireEvent('episodesDbUpdated');
-	});
-        
-        // When database is updated, refersh the episodesTable 
-	Ti.App.addEventListener('episodesDbUpdated', addNewEpisodesToTable);
+    // load Feed module
+    var Feed = require('Services/feed');
+    var episodesFeed = new Feed();
 
-        // Load all episode when opening app, to skip waiting for network
-        function loadEpisodesTable(){
+    // Load database module
+    var EpisodesDb = require('Services/db');
 
-            Ti.API.info('Loading episodes to tableView');
+    // Load episodeRow module
+    var EpisodeRow = require('ui/common/episodeRow');
 
-            // Fetch episodes from db
-            var episodes = new EpisodesDb();
-            var episodesList = episodes.getEpisodesList();
-            episodes.close();
+    // Load module for episodeRow
+    var EpisodeRow = require('ui/common/episodeRow');
 
-            // Add episodes to table
-            for(var i = 0, j = episodesList.length; i < j; i++){
-                var episode = episodesList[i];
-                var episodeRow = new EpisodeRow(episode);
+    // Create container for episodes
+    var episodesList = [];
+
+    // Create tableView for episodes
+    var table = Ti.UI.createTableView({});
+    self.add(table);
+
+    // Update Feeds
+    Ti.API.info('Fetching remote feed');
+    episodesFeed.fetchRemoteFeed();
+
+    Ti.App.addEventListener('fetchRemoteFeedFinished', function(e){
+
+        // Get the date of the newest stored episode
+        var episodes = new EpisodesDb();
+        var latestUpdate = episodes.getLatestUpdate();
+
+        // get the new episodes
+        Ti.API.debug('getting the new episodes');
+        newEpisodes = episodesFeed.getNewEpisodes(latestUpdate);
+
+        Ti.API.debug(newEpisodes.length + ' new episodes to save');
+
+        // Save new episodes to database
+        for(var i = 0, j = newEpisodes.length; i < j; i++){
+            // newEpisodes is an array with DOMElements
+            var itemNode = newEpisodes[i];
+
+            // Feed.getEpisodeDetails returns an objects
+            var episode = episodesFeed.getEpisodeDetails(itemNode);
+
+            Ti.API.debug(episode.title);
+
+            episodes.saveEpisode(episode);
+        }
+
+        // Remember to close the DB
+        episodes.close();
+
+        Ti.App.fireEvent('episodesDbUpdated');
+    });
+
+    // When database is updated, refresh the episodesTable 
+    Ti.App.addEventListener('episodesDbUpdated', addNewEpisodesToTable);
+
+    // Method to add episodes to table
+    function addEpisodesToTable(){
+
+        Ti.API.info('Adding all episodes in DB to table');
+
+        // Fetch episodes from db
+        var episodes = new EpisodesDb();
+        var episodesList = episodes.getEpisodesList();
+        episodes.close();
+
+        // Add episodes to table
+        for(var i = 0, j = episodesList.length; i < j; i++){
+            var episode = episodesList[i];
+            var episodeRow = new EpisodeRow(episode);
+            table.appendRow(episodeRow);
+        };
+    };
+
+    function addNewEpisodesToTable(){
+
+        Ti.API.info('Reloading episodes table');
+
+        // Fetch episodes from db
+        var episodes = new EpisodesDb();
+
+        // Db.getEpisodesList returns an array with objects
+        newEpisodesList = episodes.getNewEpisodesList(12387645);
+        episodes.close();
+
+        Ti.API.debug(episodesList.length + ' episodes in list');
+
+        // Add each episode to tableView
+        var episodes = episodesList.length;
+
+        // Add episodes
+        for(var i = 0, j = episodes; i < j; i++){
+            var episode = episodesList[i];
+
+            var episodeRow = new EpisodeRow(episode);
+
+            // when inserting first row, make sure there
+            // is at least one row, otherwise, create first row
+            if(table.sections.length < 1){
                 table.appendRow(episodeRow);
+            } else {
+                table.insertRowBefore(0,episodeRow);
             };
         };
-	
-	function addNewEpisodesToTable(){
-		
-		Ti.API.info('Reloading episodes table');
-		
-		// Fetch episodes from db
-		var episodes = new EpisodesDb();
-		
-		// Db.getEpisodesList returns an array with objects
-		newEpisodesList = episodes.getNewEpisodesList(12387645);
-		episodes.close();
-		
-		Ti.API.debug(episodesList.length + ' episodes in list');
+    }
 
-		// Add each episode to tableView
-		var episodes = episodesList.length;
+    // add behavior
+    table.addEventListener('click', function(e) {
+        self.fireEvent('itemSelected', {
+            title: e.rowData.episodeTitle,
+        episodeId: e.rowData.episodeId
+        });
+    });
 
-                // Add episodes
-		for(var i = 0, j = episodes; i < j; i++){
-                    var episode = episodesList[i];
+    // Load all episode when opening app, to skip waiting for network
+    addEpisodesToTable();
 
-                    var episodeRow = new EpisodeRow(episode);
-
-                    // when inserting first row, make sure there
-                    // is at least one row, otherwise, create first row
-                    if(table.sections.length < 1){
-                        table.appendRow(episodeRow);
-                    } else {
-                        table.insertRowBefore(0,episodeRow);
-                    };
-		};
-	}
-	
-	// add behavior
-	table.addEventListener('click', function(e) {
-		self.fireEvent('itemSelected', {
-                        title: e.rowData.episodeTitle,
-			episodeId: e.rowData.episodeId
-		});
-	});
-
-        loadEpisodesTable();
-	
-	return self;
+    return self;
 };
 
 module.exports = MasterView;
